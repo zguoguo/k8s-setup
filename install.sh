2f8e0986e0200a861a116bcc0e895afc79d82d19
aterrero <51141052+aterrero@users.noreply.github.com>
2019-05-29 03:32:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,5 +29,5 @@ mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-echo Finished Installation
+echo "Finished Installation"
 kubectl get nodes
