aa3f246e520a576b4a354abdc70b8ea7b950658a
aterrero <51141052+aterrero@users.noreply.github.com>
2019-05-29 03:15:02 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apt-get update
-apt-get install docker docker.io
+apt-get --asume-yes install docker docker.io
 systemctl enable docker
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
