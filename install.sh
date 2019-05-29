7108256dd04d36cff3d89b016bb1dfd7371318cb
aterrero <51141052+aterrero@users.noreply.github.com>
2019-05-29 13:04:31 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,10 @@ swapoff -a
 kubeadm init --ignore-preflight-errors='all' --pod-network-cidr=10.244.0.0/16
 kubeadm token create --print-join-command > joincommand.sh
 chmod 755 joincommand.sh
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
 cat workers | while read line
 do
@@ -24,9 +28,5 @@ do
     fi
 done
 
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 echo "Finished Installation"
 kubectl get nodes
