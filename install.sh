e94de9ac3a00daa2a2d7d7e501d9f0fc018c7613
aterrero <51141052+aterrero@users.noreply.github.com>
2019-05-29 12:53:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 apt-get update
-apt-get --asume-yes install docker docker.io
+apt-get -y install docker docker.io
 systemctl enable docker
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-apt-get --asume-yes install kubeadm
+apt-get -y install kubeadm
 swapoff -a
 kubeadm init --ignore-preflight-errors='all' --pod-network-cidr=10.244.0.0/16
 kubeadm token create --print-join-command > joincommand.sh
